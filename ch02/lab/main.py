1dc7bf53c0ef0d32d0c3fed2ed0d297d7a2a8263
Ivan Yun <112439908+iyun1@users.noreply.github.com>
2022-09-07 20:40:45 +0000

    Completed Chapter 2 Lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,13 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
-
-
+print("Cost per week ($):", cost_per_week)
+classes_per_week=3
+cost_per_class=(cost_per_week/classes_per_week)
+print("Cost Per Class ($):", cost_per_class)
 #Part B
+random_list=["Explosion", "5248964", "Wyoming", "-0", "E-Grade"]
+print ("Random List Choice:", random.choice(random_list))
