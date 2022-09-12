ae25e0d06e47c45debcde987ca2750695ca9bc7a
Ivan Yun <112439908+iyun1@users.noreply.github.com>
2022-09-12 22:05:37 +0000

    Attempted Exercise "Drawing With Turtle"

---
+++ ch02/exercises/drawturtle.py
@@ -0,0 +1,9 @@
+import turtle
+
+draw_turtle=turtle.Turtle()
+number_of_sides=input(int(Please enter number of sides.))
+length_of_sides=input(int(Please enter length of sides.))
+color=input(Please enter color.)
+print (number_of_sides)
+
+for i in (number_of_sides)
--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -1,22 +1,24 @@
 import turtle
 
 set_turtle=turtle.Turtle()
-set_turtle2=turtle.Turtle()
-window=turtle.Screen()
-set_turtle.color("purple")
-set_turtle.shape("turtle")
-
-set_turtle2.color("red")
-set_turtle2.shape("turtle")
-set_turtle2.goto(-55, 0)
-
 length=50
 angle=90
 
+
+window=turtle.Screen()
+colors=[red, purple]
+for c in colors:
+  set_turtle c
+
+
+
 for i in [angle]*4:
   set_turtle.forward(length)
-  set_turtle.left(angle)
+  set_turtle.left(angle) 
 
+set_turtle.up()
+set_turtle.forward(100
+                  )
 for i in [angle]*4:
   set_turtle2.forward(length)
   set_turtle2.left(angle)  
