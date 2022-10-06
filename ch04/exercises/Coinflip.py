261f2caaadd72c66dedc79c6443d9eff11677bbc
Ivan Yun <112439908+iyun1@users.noreply.github.com>
2022-10-06 00:31:11 +0000

    Completed Chapter 4 Exercise: Coin Flip

--- ch04/exercises/Coinflip.py
+++ ch04/exercises/Coinflip.py
@@ -1,35 +1,41 @@
 import random
 import turtle
 
-#   Write a program that behaves in the following way:
-# A turtle begins in the center of the screen.
-# Flip a coin. 
-# heads: turn to the left 90 degrees. 
-# tails: turn to the right 90 degrees.
-# Take 50 steps forward.
-# If the turtle has moved outside the screen the program stops, otherwise go back to step 2 and repeat
-
-#A few hints:
-# get the turtle moving randomly around the screen first
-# Get the turtle's coordinates to calculate if the turtle is visible on screen
-# You will need to use a while loop
-# the condition should be based on whether the turtle is visible on the screen or not 
-# Use the random library to simulate flipping a coin (50/50 odds)
-# Use an if statement to determine the direction the turtle should turn
-# move forward
-# calculate if the turtle's position is on screen again
-
+#variables
+window = turtle.Screen()
 my_turtle=turtle.Turtle()
-
 my_turtle.shape("turtle")
+window.setup(width=300, height=300, startx=0, starty=0)
+turtle.color("black")
+coinlist=[1, 2]
+
 
 outside=False
-while not outside
-coin_flip=random.randrange(0, 2, 1)
-if coin_flip==1:
-  turtle.left(90)
-  coin_flip="heads"
-else:
-  turtle.right(90)
-  coin_flip="tails"
 
+
+my_turtle.up()
+my_turtle.goto(0,0)
+while outside is False:
+  coin_flip = random.choice(coinlist)
+  print(coin_flip)
+  POS = []
+  POS = my_turtle.pos()
+  x=my_turtle.xcor()
+  y=my_turtle.ycor()
+  if x > 200:
+   break
+  if x < -200:
+    break
+  if y < -200:
+    break
+  if y > 200: 
+   break 
+  if coin_flip == 1:
+      my_turtle.left(90)
+      my_turtle.forward(50)
+  if coin_flip == 2:
+      my_turtle.right(90)
+      my_turtle.forward(50)
+
+  
+window.exitonclick()
--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -1,6 +1,20 @@
 import pygame
+import random
 
 pygame.init()
 
-#variables
+
+#dartboard variable setup
 screen=pygame.display.set_mode()
+pygame.display.get_window_size()
+dartboard=pygame.circle(surface, orange, 50, 50,)
+screen_width=500
+screen_height=500
+
+
+#commands
+random.randrange()
+
+distance_from_center = math.hypot(x1-x2, y1-y2) #the distance formula
+is_in_circle = distance_from_center <= width/2 #screen width
+
